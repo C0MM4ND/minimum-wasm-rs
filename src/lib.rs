6c9b604af3c94393867a0865362e984e2f50667a
Command <maoxs2@gmail.com>
2020-10-05 00:02:29 +0800

    add fibonacci

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,18 @@
 pub extern fn sum(x: i32, y: i32) -> i32 {
     x + y
 }
+
+#[no_mangle]
+pub extern fn fib(n: u32) -> u32 {
+    fibonacci(n)
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
